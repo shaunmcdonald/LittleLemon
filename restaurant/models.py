56aee08c41dc2e5901011bd726dbd5e5548663d2
Shaun McDonald <shaun.mcdonald.atx@gmail.com>
2024-11-01 09:08:10 -0500

    UnorderedObjectListWarning
    Have been getting this warning, so fixing it by adding ordering in the Model Classes.
    
    Also uncommented explicit declaration of allowed HTTP Methods in BookingViewSet

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,6 +6,9 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
     inventory = models.SmallIntegerField(default=0)
     
+    class Meta:
+        ordering = ['title']
+    
     def __str__(self):
       return self.title
     
@@ -13,6 +16,9 @@ class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField(default=0)
     booking_date = models.DateField()
+    
+    class Meta:
+        ordering = ['booking_date', 'name']
 
     def __str__(self): 
         return self.name
