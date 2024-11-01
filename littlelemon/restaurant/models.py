63899d5920dd081b83854185a1b38d57ae5e326b
Shaun McDonald <shaun.mcdonald.atx@gmail.com>
2024-10-31 19:39:28 -0500

    Exercise: Setup Table Booking

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,13 +6,13 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
     inventory = models.SmallIntegerField(default=0)
     
-    # def __str__(self):
-    #   return self.title
+    def __str__(self):
+      return self.title
     
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField(default=0)
     booking_date = models.DateField()
 
-#     def __str__(self): 
-#         return self.first_name
+    def __str__(self): 
+        return self.name
